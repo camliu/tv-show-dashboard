7e521e3ebb3e2db4e2bc0ec52d14985db0d3d8a4
Cam Liu <gen.zales@gmail.com>
2026-05-10 16:46:19 +0200

    chore(ci): add build step and expand workflow configs

--- app/components/layout/header/SearchOption.vue
+++ app/components/layout/header/SearchOption.vue
@@ -1,33 +1,22 @@
 <script setup lang="ts">
 const { show } = defineProps<{ show: Show }>();
-
-const loaded = ref(!show.image);
 </script>
 
 <template>
-  <div class="relative flex items-center gap-3">
-    <SearchOptionSkeleton
-      v-if="show.image && !loaded"
-      class="absolute top-0 left-0"
+  <div class="flex items-center gap-3">
+    <NuxtImg
+      v-if="show.image"
+      :src="show.image"
+      :alt="show.name"
+      width="48"
+      height="48"
+      loading="lazy"
+      class="rounded object-cover shrink-0 size-12"
     />
     <div
-      class="flex items-center gap-3"
-      :class="{ invisible: show.image && !loaded }"
-    >
-      <NuxtImg
-        v-if="show.image"
-        :src="show.image"
-        :alt="show.name"
-        width="48"
-        height="48"
-        class="rounded object-cover shrink-0 size-12"
-        @load="loaded = true"
-      />
-      <div
-        v-else
-        class="size-12 rounded bg-zinc-100 shrink-0"
-      />
-      <span>{{ show.name }}</span>
-    </div>
+      v-else
+      class="size-12 rounded bg-zinc-100 shrink-0"
+    />
+    <span>{{ show.name }}</span>
   </div>
 </template>
